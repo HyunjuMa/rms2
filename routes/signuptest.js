78b65c030fc68366dc567978540a3da6e285a74e
hyunjuma <flipped71@gmail.com>
2016-08-02 13:08:28 +0900

    l

--- routes/signuptest.js
+++ routes/signuptest.js
@@ -1,9 +1,5 @@
 //회원 가입과 관련된 라우팅
 
-var express = require('express');
-var app = express.Router();
-
-
 module.exports = function(app, User) {
 
   //Controller 관리
@@ -13,17 +9,26 @@ module.exports = function(app, User) {
   //var common = require('../../../controllers/common');
 
 
-  //GET home page
-  app.get('/', function(req, res, next) {
-    res.render('auth/register', { title: 'Register Page' });
+  app.get('/api/users', function(req,res){
+    User.find(function(err, users){
+      if(err) return res.status(500).send({error: 'db failure'});
+      res.json(users);
+    })
+  });//getall
+
+  app.get('/api/users/:user_id', function(req,res){
+    res.end();
+  });
+  app.get('/api/users/name/:name', function(req,res){
+    res.end();
   });
 
-  app.post('/', function(req,res){
+  app.post('/api/users', function(req,res){
     var user = new User();
     user.name = req.body.name;
     user.email = req.body.email;
+    user.phone = req.body.phone;
     user.pw = req.body.pw;
-//    user.imgId = req.body.image;
 
     user.save(function(err) {
       if(err) {
